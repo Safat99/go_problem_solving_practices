1c6d4bb2bcbb20d71d21e36538d61195ac9e3af2
Safat99 <sarkersafat99@gmail.com>
2023-05-09 18:07:56 +0600

    solved prob4

---
+++ prob4.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	var n int
+	fmt.Scan(&n)
+
+	if n%2 != 0 {
+		fmt.Println(-1)
+		return
+	}
+
+	nums := make([]int, n)
+
+	for i := 0; i < len(nums); i++ {
+		nums[i] = i + 1
+	}
+
+	// fmt.Println("nums: ", nums)
+
+	for i := 0; i < len(nums)-1; i++ {
+		if i%2 == 0 {
+			temp := nums[i]
+			nums[i] = nums[i+1]
+			nums[i+1] = temp
+		} else {
+			continue
+		}
+	}
+
+}
